51d12a6ce31f7f4bf4941c411d2b683c22e2a7ea
Jérémy FRERE <frere.jeremy@gmail.com>
2013-05-24 15:00:00 +0200

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmapMailer::Application.config.secret_token = '4a911548d70953561d4c09171f6a5d52f0b447bf96b5433c5fcf9011b4abdd4c81482bd589a1826230e839747b412956d59357bd46a8b0f23219d91049130743'
