8ce01b02145daf55d8eabf0b587f5a01ffb05de3
Jérémy FRERE <frere.jeremy@gmail.com>
2013-06-15 12:55:14 +0200

    Wording

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe Category do
 
-  it "shouldn't save without a name" do
+  it "should not save without a name" do
     attributes = FactoryGirl.attributes_for(:category, :name => nil)
     Category.new(attributes).should_not be_valid
   end
 
-  it "shouldn't save if name is already taken" do
+  it "should not save if name is already taken" do
     FactoryGirl.create(:category, :name => "test")
     attributes = FactoryGirl.attributes_for(:category, :name => "test")
     Category.new(attributes).should_not be_valid
--- spec/models/newsletter_spec.rb
+++ spec/models/newsletter_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe Newsletter do
 
-  it "shouldn't save without a name" do
+  it "should not save without a name" do
     attributes = FactoryGirl.attributes_for(:newsletter, :name => nil)
     Newsletter.new(attributes).should_not be_valid
   end
 
-  it "shouldn't save without a body" do
+  it "should not save without a body" do
     attributes = FactoryGirl.attributes_for(:newsletter, :body => nil)
     Newsletter.new(attributes).should_not be_valid
   end
 
-  it "shouldn't save without at least one category" do
+  it "should not save without at least one category" do
     attributes = FactoryGirl.attributes_for(:newsletter, :category_ids => [])
     Newsletter.new(attributes).should_not be_valid
   end
