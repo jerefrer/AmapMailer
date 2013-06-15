770a2b358f39e45bdb31673e660994d735addeed
Jérémy FRERE <frere.jeremy@gmail.com>
2013-06-15 12:58:46 +0200

    Wording

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -2,6 +2,6 @@
 
 FactoryGirl.define do
   factory :category do
-    sequence(:name) { |n| "Category_#{n}" }
+    sequence(:name) { |n| "Category-#{n}" }
   end
 end
--- spec/factories/subscribers.rb
+++ spec/factories/subscribers.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :subscriber do
-    sequence(:name)  { |n| "Name_#{n}" }
+    sequence(:name)  { |n| "Name-#{n}" }
     sequence(:email) { |n| "foo#{n}@example.com" }
   end
 end
