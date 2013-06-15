e2532e71ff9a71070e66b708f09e5a84c2c853ec
Jérémy FRERE <frere.jeremy@gmail.com>
2013-06-15 13:01:13 +0200

    Sets root_path to 'newsletters#index' and cleans up default index page (html+image)

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ AmapMailer::Application.routes.draw do
 
   resources :subscribers
 
-
   resources :categories
-
 
   resources :newsletters
 
-
   mount RedactorRails::Engine => '/redactor_rails'
+
+  root :to => 'newsletters#index'
 
 end
